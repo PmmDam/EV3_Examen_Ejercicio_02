ea3f9e72920d42ee4f03117fa4ef9cec23b1debf
Pablo Muñoz Martínez <699328@alu.murciaeduca.es>
2022-05-27 20:47:37 +0200

    Add project files.

---
+++ EV3_Examen_Ejercicio_02/Models/ConfigModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV3_Examen_Ejercicio_02.Models
+{
+    public class ConfigModel
+    {
+      
+        public bool Enabled { get; set; }
+        
+        private FileMode _modeForCreatingFiles { get; set; }
+        public FileMode RawModeForCreatingFiles
+        {
+            get
+            {
+                return _modeForCreatingFiles;
+            }
+            set
+            {
+                this._modeForCreatingFiles = value;
+            }
+        }
+
+        public string ModeForCreatingFiles
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(this._modeForCreatingFiles.ToString()) ? this._modeForCreatingFiles.ToString() : string.Empty;
+                
+            }
+            set
+            {
+                this._modeForCreatingFiles = (FileMode)Enum.Parse(typeof(FileMode), value);
+            }
+        }
+
+
+
+
+        private DateTime _nextBackup { get; set; }
+        public DateTime RawNextBackup
+        {
+            get
+            {
+                return this._nextBackup;
+            }
+            set
+            {
+                this._nextBackup = value;
+            }
+
+        }
+        public string NextBackup
+        {
+            get
+            {
+                return this._nextBackup != null ? this._nextBackup.ToString() : string.Empty;
+            }
+            set
+            {
+                this._nextBackup = DateTime.Parse(value);
+            }
+        }
+
+
+    }
+}
---
+++ EV3_Examen_Ejercicio_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using EV3_Examen_Ejercicio_02.Models;
+using System.Text.Json;
+
+namespace EV3_Examen_Ejercicio_02
+{
+     
+    internal class Program
+    {
+        private static string _jsonPath = Path.Combine(Environment.CurrentDirectory, "Data", "AppConfig.json");
+
+        private static void Main(string[] args)
+        {
+
+            string jsonText = File.ReadAllText(_jsonPath);
+            ConfigModel config = JsonSerializer.Deserialize<ConfigModel>(jsonText);
+
+            Console.WriteLine(config.Enabled);
+            
+            Console.WriteLine($"Esta es la Raw {config.RawNextBackup}");
+            Console.WriteLine($"Este es el string {config.NextBackup}");
+
+
+            Console.WriteLine($"Esta es la Raw {config.RawModeForCreatingFiles}");
+            Console.WriteLine($"Este es el string {config.ModeForCreatingFiles}");
+
+        }
+    }
+}
